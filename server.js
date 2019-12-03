3eeac4cde385c49a9b717df61dc12cc9275d686d
Joel Buentello <joel.buentello@yahoo.com>
2019-12-03 14:16:07 -0600

    add index.handlebars to views folder

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 // Set up Dependencies
+var mongojs = require("mongojs");
+var axios = require ("axios");
+var express = require ("express");
+var cheerio = require("cheerio");
+var exphbs = require ("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 3000
+app.use(express.static("public"));
+
 //Handlebars templating setup
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var dataCollect = ["collectedData"];
+
 // Change mongojs to db var
+var db = mongojs(process.env.MONGODB_URI || 'scraper', dataCollect);
+db.on("error", function (error) {
+  console.log("Database:", error);
+});
+//Render the data into index.handlebars
+app.get("/", function(req, res) {
+  res.render("index");
+})
+
 // Use axios (app.get) to pull data from site
